5d7a9d15ac3b4d6c4a39f1d4e85abfe654123abd
250630 <250630@PC332512.cts.com>
2018-04-13 16:48:39 +0530

    Selenium grid code

---
+++ src/main/java/gridTest/Gridtest.java
@@ -0,0 +1,42 @@
+package gridTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class Gridtest {
+
+	public static void main(String[] args) throws MalformedURLException {
+		
+	//Set the desired capabilities for the nodes
+		
+	DesiredCapabilities descap = new DesiredCapabilities();
+	descap.setBrowserName("chrome");;
+	descap.setPlatform(Platform.WIN10);
+
+	//Chrome Options Definition
+	
+	ChromeOptions options = new ChromeOptions();
+	
+	// Merge the desired Capabilities with Chrome options
+	
+	options.merge(descap);
+	
+	descap.setBrowserName("firefox");
+	
+	
+	String huburl= "http://localhost:4444/wd/hub";
+	
+	//Remote WebDriver is always used to execute scripts on a remote server 
+	WebDriver driver = new RemoteWebDriver(new URL(huburl),options);
+	
+	driver.get("https://www.google.com");
+	System.out.println(driver.getTitle());
+	}
+
+}
